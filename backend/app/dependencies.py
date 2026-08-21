095c3593c64178c292e954872bcea399bc07f0f8
mobious952004 <pranjalgupta@ce.du.ac.in>
2026-08-21 23:17:01 +0530

    Embeddings

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -1,41 +1,41 @@
-# app/dependencies.py
+# # app/dependencies.py
+
+# # from app.services.embedding import EmbeddingService
+# # from app.services.vectorstore import VectorStoreService
+# # from app.services.retriever import RetrieverService
+
+# # embeddings = EmbeddingService().get_embeddings()
+# # vector_store = VectorStoreService(embeddings)
+# # retriever_service = RetrieverService(vector_store)
 
 # from app.services.embedding import EmbeddingService
 # from app.services.vectorstore import VectorStoreService
 # from app.services.retriever import RetrieverService
 
-# embeddings = EmbeddingService().get_embeddings()
-# vector_store = VectorStoreService(embeddings)
-# retriever_service = RetrieverService(vector_store)
 
-from app.services.embedding import EmbeddingService
-from app.services.vectorstore import VectorStoreService
-from app.services.retriever import RetrieverService
+# _embeddings = None
+# _vector_store = None
+# _retriever = None
 
 
-_embeddings = None
-_vector_store = None
-_retriever = None
+# def get_retriever():
 
+#     global _embeddings
+#     global _vector_store
+#     global _retriever
 
-def get_retriever():
+#     if _retriever is None:
 
-    global _embeddings
-    global _vector_store
-    global _retriever
+#         print("Creating dependencies...")
 
-    if _retriever is None:
+#         _embeddings = EmbeddingService().get_embeddings()
 
-        print("Creating dependencies...")
+#         _vector_store = VectorStoreService(
+#             _embeddings
+#         )
 
-        _embeddings = EmbeddingService().get_embeddings()
+#         _retriever = RetrieverService(
+#             _vector_store
+#         )
 
-        _vector_store = VectorStoreService(
-            _embeddings
-        )
-
-        _retriever = RetrieverService(
-            _vector_store
-        )
-
-    return _retriever
+#     return _retriever
--- backend/app/services/embedding.py
+++ backend/app/services/embedding.py
@@ -1,38 +1,17 @@
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain_huggingface import HuggingFaceEmbeddings
 from app import config  
 import os
 
 
-# class EmbeddingService:
-
-#     def __init__(self):
-#         # api_key = os.getenv("GOOGLE_API_KEY")
-#         # if not api_key:
-#         #     raise ValueError("GOOGLE_API_KEY is not set. Check your .env file.")
-#         # self.embeddings = GoogleGenerativeAIEmbeddings(
-#         #     model="models/gemini-embedding-001"
-#         # )
-#         self.embeddings = HuggingFaceEmbeddings(
-#             model_name="sentence-transformers/all-MiniLM-L6-v2"
-#         )
-
-#     def get_embeddings(self):
-#         return self.embeddings
-
-
 class EmbeddingService:
 
-    _embeddings = None
+    def __init__(self):
+        api_key = os.getenv("GOOGLE_API_KEY")
+        if not api_key:
+            raise ValueError("GOOGLE_API_KEY is not set. Check your .env file.")
+        self.embeddings = GoogleGenerativeAIEmbeddings(
+            model="models/gemini-embedding-001"
+        )
 
-    @classmethod
-    def get_embeddings(cls):
-
-        if cls._embeddings is None:
-            print("Loading embedding model...")
-
-            cls._embeddings = HuggingFaceEmbeddings(
-                model_name="sentence-transformers/all-MiniLM-L6-v2"
-            )
-
-        return cls._embeddings
+    def get_embeddings(self):
+        return self.embeddings
